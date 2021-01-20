704cc2ce2174aea5018ffccbde6254189ffc5a34
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-20 22:37:22 +0100

    update select

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, {Fragment} from 'react';
+import React, { Fragment} from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <Fragment>
-      <Header 
+      <Header
         titulo='Clima React App'
       />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
