cde6e33c44bc6b20684a4a361b9850be7737e09f
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-21 22:11:36 +0100

    add result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
 
   //State para no consultar la API todo el tiempo => dependecia consultar
   const [ consultar, guardarConsultar ] = useState(false);
+  const [ resultado, guardarResultado ] = useState({});
 
   //Extraer datos
   const { ciudad, pais } = busqueda;
@@ -26,7 +28,10 @@ function App() {
           const respuesta = await fetch(url);
           const resultado = await respuesta.json();
     
-          console.log(resultado);
+          guardarResultado(resultado);
+
+          //Para poder realizar otra consulta
+          guardarConsultar(false);
         }
 
     }
@@ -50,7 +55,9 @@ function App() {
               />
             </div>
             <div className="col m6 s12">
-              2
+              <Clima
+                resultado={resultado}
+              />
             </div>
           </div>
         </div>
